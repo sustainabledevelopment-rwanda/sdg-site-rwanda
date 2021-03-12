e08b24dbde8310f65693e24c86cdd5c76126c1b9
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-03-12 18:03:35 +0000

    Deploying to gh-pages from  @ 3ef55427ec6392856cc44deda29f870a23d8f4f2 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1016,7 +1016,7 @@ var GEOCODE_COLUMN = 'GeoCode';
 var YEAR_COLUMN = 'Year';
 var VALUE_COLUMN = 'Value';
 var HEADLINE_COLOR = '#777777';
-var SERIES_TOGGLE = true;
+var SERIES_TOGGLE = false;
 
   /**
  * Model helper functions with general utility.
