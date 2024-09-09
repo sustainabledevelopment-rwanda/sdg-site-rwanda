f1f2a2b0ce81d9a4e82efbf37889d680c60a2416
sarah-beasley <106670682+sarah-beasley@users.noreply.github.com>
2024-09-09 13:08:19 +0000

    Deploying to gh-pages from  @ 708ea2ae87e4d590b0ee3e0d5e8487c61eb70d5c 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3298,7 +3298,7 @@ var mapView = function () {
     $('#map').sdgMap({
       indicatorId: indicatorId,
       mapOptions: {"disaggregation_controls":false,"minZoom":5,"maxZoom":10,"tileURL":"","tileOptions":{"id":"","accessToken":"","attribution":""},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":55}},
-      mapLayers: [],
+      mapLayers: [{"subfolder":"geojsons","label":"Regions","min_zoom":5,"max_zoom":10,"staticBorders":false}],
       precision: precision,
       precisionItems: precisionItems,
       decimalSeparator: decimalSeparator,
